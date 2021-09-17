0132a84e5637628de51a46b037bac0a1ff709250
nbalint93 <nagybalint93@gmail.com>
2021-09-17 20:23:30 +0200

    asd

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,15 @@
+/*
 let country = "Hungary";
 let continent = "Europe";
-let population = "10 million";
+let population = 10;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
